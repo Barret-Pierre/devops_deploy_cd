efb55bd727cbe09a5d30ce6b43e45927f1de2958
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-03-12 19:14:21 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.ami
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  tags = {
+    Name = "aws_docker_nginx"
+  }
+  vpc_security_group_ids = [ var.vpc_security_group_id ]
+}
+
+resource "null_resource" "install_docker" {
+  depends_on = [aws_instance.app_server]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"  # Faire attention, change en fonction des AIM
+    private_key = var.private_key 
+    host        = aws_instance.app_server.public_ip
+  }
+
+  provisioner "file" {
+    source      = "./install_docker.sh"  # Chemin de la source
+    destination = "/tmp/install_docker.sh"  # Le chemin sur l'instance EC2 où copier le fichier
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/install_docker.sh", # Assure que le scrypt soit executable
+      "/tmp/install_docker.sh" # Exécute le script shell
+    ]
+  }
+}
+
+resource "null_resource" "deploy_nginx" {
+  depends_on = [null_resource.install_docker]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"  
+    private_key = var.private_key
+    host        = aws_instance.app_server.public_ip
+  }
+
+  provisioner "file" {
+    source      = "./docker-compose.yml" 
+    destination = "/home/ec2-user/docker-compose.yml" 
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker-compose -f /home/ec2-user/docker-compose.yml up --build -d"
+    ]
+  }
+}
+
+ 
+
+
---
+++ varirables.tf
@@ -0,0 +1,29 @@
+variable "ami" {
+  description = "Value of the ami"
+  type        = string
+  default = "ami-0b7282dd7deb48e78"
+}
+
+variable "region" {
+  description = "Value of the cluster region"
+  type        = string
+  default = "eu-west-3"
+}
+
+variable "key_name" {
+  description = "Value of the key_name"
+  type        = string
+  default = "tp_devops"
+}
+
+variable "private_key" {
+  description = "Value of the private key"
+  type        = string
+}
+
+variable "vpc_security_group_id" {
+  description = "Value of the secutiry group id"
+  type        = string
+  default = "sg-0b382662fbf5c3b45"
+}
+
